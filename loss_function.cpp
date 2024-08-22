40ebad05f143cb14aa751a1fd5633f522a782413
Jhon68h <jhon.68h@gmail.com>
2024-08-22 09:37:29 -0500

    creación de la función cross entropy

--- loss_function.cpp
+++ loss_function.cpp
@@ -1,9 +1,9 @@
 #include "include/loss_functions.hpp"
+#include <cmath>
 #include <iterator>
+#define epsilon 1e-10f
 
-
-//This function visualize the bigest probability and takes that position like
-//the prediction
+//This function visualize the largest probability and takes that position as the prediction
 vector<int> Loss::prediction(vector<vector<float>> x){
 
     vector<int> predicted_numbers_list;
@@ -18,6 +18,11 @@ vector<int> Loss::prediction(vector<vector<float>> x){
 }
 
 vector<vector<int>> distributionVector(vector<int> labels){
+    //La función tiene que leer el label y en un vector de 0 de 10 posiciones
+    //pondrá un 1 en la posicion que el label marque
+
+    //EJEMPLO -> {5}{0, 0, 0, 0, 0, 1, 0, 0, 0, 0}
+
     int cols = 10;
     int value = 1;
     auto rows = labels.size();
@@ -33,8 +38,6 @@ vector<vector<int>> distributionVector(vector<int> labels){
     return positionVector;
 }
 
-//{5}{0, 0, 0, 0, 0, 1, 0, 0, 0, 0}
-
 
 // cross entropy
 // H(p, q) = -SUM i->M p(i)log(q(i))
@@ -42,3 +45,18 @@ vector<vector<int>> distributionVector(vector<int> labels){
 // p(i) probabilidad verdadera del evento i
 // q(i) probabilidad predicha por el modelo
 
+
+auto cross_entropy(const vector<vector<int>>& distributionVector, const vector<vector<float>>& predictions){
+    auto distributionVectorSize = distributionVector.size();
+
+    float sum = 0.0f;
+
+    #pragma omp parallel for reduction(-:sum) // Paralelización del bucle
+    for(int i = 0; i < distributionVectorSize; i++) {
+        for(int j = 0; j < 10; j++) {
+            sum -= distributionVector[i][j] * log(predictions[i][j] + epsilon);
+        }
+    }
+
+    return sum;
+}
