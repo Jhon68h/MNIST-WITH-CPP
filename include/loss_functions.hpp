90a99e0235e730bfb935c3f77bd8a771f71aea8b
jhonatan <jhonatan@debian.debian>
2024-09-02 17:33:51 -0500

    Se completó la función de loss function, falta graficarla y actualizar los valores en cada epoca

--- include/loss_functions.hpp
+++ include/loss_functions.hpp
@@ -11,18 +11,19 @@ using namespace std;
 class Loss {
 
     public:
-        // Constructor que inicializa los miembros de referencia 'labels' y 'probabilities'
-        Loss(const vector<int>& labels, const vector<vector<float>>& probabilities);
+
+        Loss(int epoch, const vector<int>& labels, const vector<vector<float>>& probabilities);
 
         // Función que convierte los labels en un vector de distribución binario
         vector<vector<int>> distributionVector(const vector<int>& labels);
         // Función que calcula la entropía cruzada dada una distribución y predicciones
-        float cross_entropy(const vector<vector<int>>& distributionVector, vector<vector<float>> prediction);
+        double cross_entropy(const vector<vector<int>>& distributionVector, vector<vector<float>> prediction);
 
     private:
         const vector<int>& labels;  // Referencia constante a los labels
         const vector<vector<float>>& probabilities;  // Referencia constante a las probabilidades
-        // Las siguientes variables fueron eliminadas ya que no eran necesarias:
-        // vector<vector<float>> x;
-        // vector<vector<float>> prediction;
+        int epoch; //Epocas 
+        vector<float> lossValues; //Vector para almacenar el valor de la funcion de perdidad en cada epoca
+
 };
+    
--- loss_function.cpp
+++ loss_function.cpp
@@ -2,34 +2,26 @@
 #include <cmath>
 #include <iterator>
 #include <vector>
+
 #define epsilon 1e-10f
 
 // Constructor de la clase Loss, inicializa las referencias 'labels' y 'probabilities' usando la lista de inicialización.
-Loss::Loss(const vector<int>& labels, const vector<vector<float>>& probabilities)
-    : labels(labels), probabilities(probabilities) {  // Usa la lista de inicialización
+Loss::Loss(int epoch, const vector<int>& labels, const vector<vector<float>>& probabilities)
+    : epoch(epoch), labels(labels), probabilities(probabilities) {  // Usa la lista de inicialización
 
     // Utiliza los miembros de la clase en lugar de crear nuevas variables
     vector<vector<int>> distributionVectorVariable = distributionVector(labels);
 
     // Calcula la entropía cruzada usando los vectores distribuidos y las probabilidades
-    auto crossEntropy = cross_entropy(distributionVectorVariable, probabilities);
-}
-
-/*
-// Esta función visualiza la probabilidad más alta y toma esa posición como la predicción
-vector<int> Loss::prediction(vector<vector<float>> x){
-
-    vector<int> predicted_numbers_list;
-
-    for(auto& row : x){
-        auto maxElement = max_element(row.begin(), row.end()); // Busca el elemento máximo en la fila
-        auto predicted_numbers = distance(row.begin(), maxElement); // Marca esa posición
-        
-        predicted_numbers_list.push_back(predicted_numbers);
+    lossValues.resize(epoch);
+    for(int i = 0; i < epoch; i++){
+        auto crossEntropy = cross_entropy(distributionVectorVariable, probabilities);
+        lossValues[i] = crossEntropy;
     }
-    return predicted_numbers_list;
+
 }
-*/
+
+
 
 vector<vector<int>> Loss::distributionVector(const vector<int>& labels){
     // La función tiene que leer el label y en un vector de 0 de 10 posiciones
@@ -51,13 +43,13 @@ vector<vector<int>> Loss::distributionVector(const vector<int>& labels){
     return positionVector;
 }
 
-float Loss::cross_entropy(const vector<vector<int>>& distributionVector, vector<vector<float>> prediction) {
+double Loss::cross_entropy(const vector<vector<int>>& distributionVector, vector<vector<float>> prediction) {
     // Se ingresa el distributionVector, este vector fue implementado en la anterior
     // función, que como se explicó, es un vector que determina la posición
     // real del label
     auto distributionVectorSize = distributionVector.size();
 
-    float sum = 0.0f;
+    double sum = 0.0f;
 
     #pragma omp parallel for reduction(-:sum) // Paralelización del bucle
     for(int i = 0; i < distributionVectorSize; i++) {
