e9cbb9231a620e3374eb563482e2e3d0a86b0516
jhonatan <jhonatan@debian.debian>
2024-09-03 15:18:27 -0500

    se borró la clase de backpropagation, esto debido a un mal entendido con los optimizadores, se movieron las derivadas al archivo de activations_functions y se implementará una clase para los optimizadores

--- activation_functions.cpp
+++ activation_functions.cpp
@@ -1,7 +1,7 @@
 #include <cmath>
 #include <algorithm> 
 #include <vector>
-
+#include <iostream>
 
 using namespace std;
 
@@ -32,4 +32,49 @@ void softmax(vector<vector<float>>& input) {
             value /= sum_exp;
         }
     }
+}
+
+vector<float> derivateRelu(vector<float> input) {
+    auto sizeInput = input.size(); 
+    vector<float> derivate;
+    
+    for (size_t i = 0; i < sizeInput; i++) {
+        derivate[i] = (input[i] > 0) ? 1.0f : 0.0f;
+    }
+    return derivate;
+}
+
+vector<vector<float>> derivateSoftmax(vector<vector<float>> softmaxVector, vector<vector<int>> distributionVector){
+    /*se necesita calcular la derivada de la función de perdidad
+    con respecto a los logits z_i que entran en la función de softmax
+    */
+    //Kronecker delta
+    
+    //Por el delta de Kronecjer [i = j] = 1 --->  soft(z_i)*(1-soft(z_i)) 
+
+    //Por el delta [i != j] = 0 ---> -soft(z_i)*soft(z_j)
+    
+    //Entonces la derivada vendria siendo softmax(i) - y_i donde y_i es 1 para la clase
+    //correcta y 0 para la incorrecta
+
+    /* Se necesita calcular la derivada de la función de pérdida
+    con respecto a los logits z_i que entran en la función de softmax. */
+
+    ///////////////////////////QUE RECIBE?/////////////////////////////
+
+    //recibe el valor de softmax
+    //recibe el vector de valores correctos
+    auto row = softmaxVector.size();
+    auto col = softmaxVector[0].size();
+
+    vector<vector<float>> derivateVector(row, vector<float>(col, 0));
+
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++){
+            derivateVector[i][j] = softmaxVector[i][j] - distributionVector[i][j];
+        }
+    }
+
+    return derivateVector;
+
 }
